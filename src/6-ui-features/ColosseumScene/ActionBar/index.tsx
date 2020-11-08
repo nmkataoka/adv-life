6a3af6926a438b2a23a10710e3761e12bb1386ea
Nolan Kataoka <nmkataoka@gmail.com>
2020-11-08 14:06:28 -0500

    Add ActionBar to the Colosseum scene [Re: nmkataoka/adv-life#166]
    Instead of porting over the action bar from the combat scene, I just
    made a new one. It's not hooked up to the backend yet.

---
+++ src/6-ui-features/ColosseumScene/ActionBar/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import ActionButton from './ActionButton';
+
+const actions = [
+  { name: 'attack', displayText: 'Attack' },
+  { name: 'defend', displayText: 'Defend' },
+  { name: 'fireball', displayText: 'Fireball', aoeRadius: 1 },
+  { name: 'stealth', displayText: 'Stealth' },
+  { name: 'heal', displayText: 'Heal' },
+  { name: 'flee', displayText: 'Flee' },
+];
+
+export default function ActionBar(): JSX.Element {
+  return (
+    <Container>
+      {actions.map(({ name, displayText }) => (
+        <ActionButton key={name} displayText={displayText} />
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #060708;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 0.5em 0.25em;
+  position: absolute;
+  bottom: 0;
+`;
--- src/6-ui-features/ColosseumScene/index.tsx
+++ src/6-ui-features/ColosseumScene/index.tsx
@@ -1,11 +1,13 @@
 import { RootState } from '7-app/types';
 import React from 'react';
+import styled from '@emotion/styled';
 import { useSelector as useReduxSelector } from 'react-redux';
 import { useSelector } from '4-react-ecsal';
 import { Canvas } from 'react-three-fiber';
 import { getUnitPosition } from '3-frontend-api';
 import Ground from './Ground';
 import Unit from './Unit';
+import ActionBar from './ActionBar';
 
 export default function ColosseumScene(): JSX.Element {
   const playerId = useReduxSelector((state: RootState) => state.player.playerId);
@@ -14,13 +16,22 @@ export default function ColosseumScene(): JSX.Element {
   const playerPosition: [number, number, number] = [playerPos.x, playerPos.y, 0];
 
   return (
-    <Canvas>
-      <Ground position={[0, 0, -1]} />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Unit position={playerPosition} />
-      <Unit position={[1.2, 0, 0]} color="red" />
-      <Unit position={[4.2, 0, 0]} color="red" />
-    </Canvas>
+    <Container>
+      <Canvas>
+        <Ground position={[0, 0, -1]} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Unit position={playerPosition} />
+        <Unit position={[1.2, 0, 0]} color="red" />
+        <Unit position={[4.2, 0, 0]} color="red" />
+      </Canvas>
+      <ActionBar />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 800px;
+  width: 100%;
+  position: relative;
+`;
