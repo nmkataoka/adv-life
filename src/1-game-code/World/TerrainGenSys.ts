2453475442d029c673a9ccd9a37ded9e60df4b48
Nolan Kataoka <nmkataoka@gmail.com>
2020-12-04 00:33:47 -0500

    Update TerrainGenSys to use createEventListener [Re: nmkataoka/adv-life#236]

--- src/1-game-code/World/TerrainGenSys.ts
+++ src/1-game-code/World/TerrainGenSys.ts
@@ -1,22 +1,37 @@
-import { ECSystem, EventCallbackArgs, EventSys } from '0-engine';
+import { createEventListener, ECSystem, EventSys } from '0-engine';
 import { WorldMapCmpt } from './WorldMapCmpt';
 import { WorldMap } from './WorldMap';
 import { createRandomTerrain } from './createRandomTerrain';
 import { CREATE_NOISED_WORLD_MAP } from './constants';
 
-const createNoisedWorldMap = ({ eMgr }: EventCallbackArgs<void>) => {
+const createNoisedWorldMapSlice = createEventListener({
+  writeCmpts: [WorldMapCmpt],
+})<undefined>(function createNoisedWorldMap({
+  eMgr,
+  componentManagers: {
+    writeCMgrs: [worldMapMgr],
+  },
+}) {
   const worldMap = eMgr.createEntity('worldMap');
   const worldMapCmpt = new WorldMapCmpt();
   worldMapCmpt.data.dataLayers[WorldMap.Layer.Elevation] = createRandomTerrain();
-  eMgr.addCmpt(worldMap, worldMapCmpt);
-};
+  worldMapMgr.add(worldMap, worldMapCmpt);
+});
 
 export class TerrainGenSys extends ECSystem {
   public Start = (): void => {
-    this.eMgr.getSys(EventSys).RegisterListener(CREATE_NOISED_WORLD_MAP, createNoisedWorldMap);
+    this.eMgr
+      .getSys(EventSys)
+      .RegisterListener(CREATE_NOISED_WORLD_MAP, createNoisedWorldMapSlice.eventListener);
 
     // TODO: remove
-    createNoisedWorldMap({ eMgr: this.eMgr, payload: undefined });
+    void createNoisedWorldMapSlice.eventListener.callback({
+      eMgr: this.eMgr,
+      componentManagers: createNoisedWorldMapSlice.eventListener.componentDependencies.getComponentManagers(
+        this.eMgr,
+      ),
+      payload: undefined,
+    });
   };
 
   public OnUpdate = (): void => {};
