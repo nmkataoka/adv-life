fe5d6ebb2215cf85ba57b18b9c3bc9ea2c8b8c67
Nolan Kataoka <nmkataoka@gmail.com>
2020-12-29 23:31:08 -0500

    Add waitFor statements after fireEvent in useZoomOnScroll test
    Still getting intermittent failures, seems most likely to be related
    to async tests.

--- src/5-react-components/useZoomOnScroll.test.tsx
+++ src/5-react-components/useZoomOnScroll.test.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { screen, fireEvent, render, waitFor } from '@testing-library/react';
 import useZoomOnScroll from './useZoomOnScroll';
 
 function TestCanvas({ max, min }: { max?: number; min?: number }) {
@@ -15,20 +15,23 @@ function TestCanvas({ max, min }: { max?: number; min?: number }) {
 describe('useZoomOnScroll', () => {
   beforeEach(() => {});
 
-  it('starts at scale=1 and zooms in when scrolling down', () => {
+  it('starts at scale=1 and zooms in when scrolling down', async () => {
     const { container, getByTestId } = render(<TestCanvas />);
     expect(container).toHaveTextContent('1.0');
 
     fireEvent.wheel(getByTestId('canvas'), { deltaY: 10000 });
+    await waitFor(() => screen.getByText('2.0'));
     expect(container).toHaveTextContent('2.0');
   });
 
-  it('does not zoom past default min and max', () => {
+  it('does not zoom past default min and max', async () => {
     const { container, getByTestId } = render(<TestCanvas />);
     fireEvent.wheel(getByTestId('canvas'), { deltaY: 100000 });
+    await waitFor(() => screen.getByText('2.0'));
     expect(container).toHaveTextContent('2.0');
 
     fireEvent.wheel(getByTestId('canvas'), { deltaY: -100000 });
+    await waitFor(() => screen.getByText('1.0'));
     expect(container).toHaveTextContent('1.0');
   });
 });
