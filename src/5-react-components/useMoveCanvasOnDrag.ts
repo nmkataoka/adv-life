2a456cabad92e4022dcbe35a4ac65d76f40c4071
Nolan Kataoka <nmkataoka@gmail.com>
2021-05-23 14:04:35 -0400

    Refactor map dragging code to prepare for clamping [Re: nmkataoka/adv-life#263]
    Refactored the map dragginc code in preparation to add translation
    clamping (prevent moving past the map edges). This refactor smells good
    because it tweaked the state variables such that exactly one is set per
    mouse event. Previously I had some doubled up setStates which probably
    indicated dependent state variables.

--- src/5-react-components/useMoveCanvasOnDrag.ts
+++ src/5-react-components/useMoveCanvasOnDrag.ts
@@ -1,44 +1,41 @@
+import { Vector2 } from '8-helpers/math';
 import React, { useState, RefObject, MouseEventHandler } from 'react';
 
 export function useMoveCanvasOnDrag(canvasRef: RefObject<HTMLCanvasElement>): {
   onMouseDown: MouseEventHandler;
   onMouseUp: MouseEventHandler;
   onMouseMove: MouseEventHandler;
-  translation: { x: number; y: number };
+  translation: Vector2;
 } {
-  const [lastMousePos, setLastMousePos] = useState<React.MouseEvent | undefined>(undefined);
-  const [translation, setTranslation] = useState({ x: 0, y: 0 });
+  const [startMousePos, setStartMousePos] = useState<Vector2 | undefined>(undefined);
+  const [translation, setTranslation] = useState(new Vector2(0, 0));
 
   const onMouseDown = (e: React.MouseEvent) => {
     e.preventDefault();
-    setLastMousePos(e);
+    setStartMousePos(new Vector2(e.clientX, e.clientY));
   };
 
   const onMouseUp = () => {
-    setLastMousePos(undefined);
+    setStartMousePos(undefined);
   };
 
   const onMouseMove = (e: React.MouseEvent) => {
-    if (lastMousePos) {
+    if (startMousePos) {
       e.preventDefault();
-      const { clientX: startX, clientY: startY } = lastMousePos;
-
-      // For some reason this value seems to be half of the `scale` in useWorldMap
+      const { x: startX, y: startY } = startMousePos;
       const canvasScale = canvasRef.current?.getContext('2d')?.getTransform().a ?? 1;
 
       // Assume no zooming happened during the drag event
-      const dragX = (e.clientX - startX) / canvasScale;
-      const dragY = (e.clientY - startY) / canvasScale;
+      const newTranslationX = (e.clientX - startX) / canvasScale;
+      const newTranslationY = (e.clientY - startY) / canvasScale;
+
+      // TODO: clamp translation
 
       const ctx = canvasRef.current?.getContext('2d');
       if (ctx) {
-        ctx.translate(dragX, dragY);
+        ctx.translate(newTranslationX - translation.x, newTranslationY - translation.y);
       }
-      setLastMousePos(e);
-      setTranslation({
-        x: translation.x + dragX,
-        y: translation.y + dragY,
-      });
+      setTranslation(new Vector2(newTranslationX, newTranslationY));
     }
   };
 
